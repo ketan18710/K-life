6c964865b32b97f88b6f7841db8dc100d79c48cf
ketan18710 verma <ketan.verma@binaryprospects.com>
2021-03-06 00:38:51 +0530

    gitignore : added local vscode changes

---
+++ src/containers/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './style.scss'
+import Header from '../../components/Header/index'
+function App() {
+  return (
+    <div>
+        <Header />    
+    </div>
+  )
+}
+
+export default App
