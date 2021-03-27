38c1a38672be44eb16df233ab1c2dfd9908617d5
kanavbhatia <kanavbhatia97@egmail.com>
2021-03-27 16:18:40 +0530

    Node server with POST working

--- Node Server/index.js
+++ Node Server/index.js
@@ -5,29 +5,46 @@ const data = require('./data.json');
 const app = express();
 const port = process.env.port || 3000;
 
+app.use(express.json());
+
 app.get('/api', (req, res) => {
   //   console.log(os.arch());
   res.send(data);
 });
 
-app.get('/:firstName/:secondName/:age', (req, res) => {
-  console.log(os.arch());
-  res.send(req.params);
+// app.get('/:firstName/:secondName/:age', (req, res) => {
+//   console.log(os.arch());
+//   res.send(req.params);
+// });
+
+// app.put('/changes', data => {
+//   fs.writeFile(
+//     'data.json',
+//     JSON.stringify({
+//       firstName: 'kanav',
+//       secondName: 'bhatia',
+//       age: '23',
+//     }),
+//     function(err) {
+//       if (err) throw err;
+//       console.log('Saved!');
+//     },
+//   );
+// });
+
+app.post('/push', (req, res) => {
+  console.log(req.body);
+  if (req.body) {
+    fs.writeFile('./data.json', JSON.stringify(req.body), err => {
+      console.log(err);
+      res.status(422).send(err);
+    });
+    res.send('Data Fetched Successfully');
+  } else res.send('Incorrect data format');
 });
 
-app.put('/changes', data => {
-  fs.writeFile(
-    'data.json',
-    JSON.stringify({
-      firstName: 'kanav',
-      secondName: 'bhatia',
-      age: '23',
-    }),
-    function(err) {
-      if (err) throw err;
-      console.log('Saved!');
-    },
-  );
+app.get('*', (req, res) => {
+  res.status(404).send('Invalid API');
 });
 
 app.listen(port, () => console.log(`RUNNING ON PORT ${port}`));
